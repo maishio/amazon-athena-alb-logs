37a0e76411132eb20f41a376ac64122aedcad094
maishio <masanori.ishio119@gmail.com>
2023-04-26 10:57:27 +0900

    refactor: rename s3 module to alb_logs module

--- terraform/modules/alb/alb_logs.tf
+++ terraform/modules/alb/alb_logs.tf
@@ -1,13 +1,13 @@
-module "s3" {
+module "alb_logs" {
   source        = "../../resources/s3/bucket"
   bucket        = "${var.tags.service}-${var.tags.env}-alb-logs-${var.account.id}"
   force_destroy = true
   tags          = var.tags
 }
 
-module "s3_lifecycle" {
+module "alb_logs_lifecycle" {
   source = "../../resources/s3/bucket_lifecycle_configuration"
-  bucket = module.s3.s3_bucket.bucket
+  bucket = module.alb_logs.s3_bucket.bucket
   rule = [
     {
       id     = "alb_logs"
@@ -27,12 +27,12 @@ module "s3_lifecycle" {
   ]
 }
 
-module "s3_policy" {
+module "alb_logs_policy" {
   source = "../../resources/s3/bucket_policy"
-  bucket = module.s3.s3_bucket.bucket
+  bucket = module.alb_logs.s3_bucket.bucket
   path   = "${path.module}/files/template/alb_s3_bucket_policy.json.tpl"
   vars = {
     ACCOUNT = data.aws_elb_service_account.this.arn
-    BUCKET  = module.s3.s3_bucket.id
+    BUCKET  = module.alb_logs.s3_bucket.id
   }
 }

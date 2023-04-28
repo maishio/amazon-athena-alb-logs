1e2cbfe213af6dfab9023b64de8b193a54c729b6
maishio <masanori.ishio119@gmail.com>
2023-04-29 08:50:20 +0900

    feat: add alb_logs_named_query module

--- terraform/modules/athena/athena.tf
+++ terraform/modules/athena/athena.tf
@@ -1,4 +1,4 @@
-module "athena_workgroup" {
+module "alb_logs_workgroup" {
   source            = "../../resources/athena/workgroup"
   description       = "Athena workgroup for ALB logs"
   encryption_option = "SSE_S3"
@@ -8,10 +8,24 @@ module "athena_workgroup" {
   tags              = var.tags
 }
 
-module "athena_database" {
+module "alb_logs_database" {
   source        = "../../resources/athena/database"
   bucket        = module.s3.s3_bucket.id
   comment       = "Athena database for ALB logs"
   force_destroy = true
   name          = "alb_logs"
+}
+
+module "alb_logs_named_query" {
+  source      = "../../resources/athena/named_query"
+  database    = module.alb_logs_database.athena_database.id
+  description = "Athena named query for ALB logs"
+  name        = "alb_logs"
+  path        = "${path.module}/files/template/create_alb_logs_table.sql.tpl"
+  vars = {
+    ACCOUNT            = var.account.id
+    ALB_LOGS_BUCKET_ID = var.alb_logs_bucket_id
+    REGION             = var.region.id
+  }
+  workgroup = module.alb_logs_workgroup.athena_workgroup.id
 }
--- terraform/modules/athena/variables.tf
+++ terraform/modules/athena/variables.tf
@@ -3,7 +3,27 @@ variable "account" {
   type        = map(string)
 }
 
+variable "alb_logs_bucket_id" {
+  description = "ALB logs bucket ID."
+  type        = string
+}
+
+variable "flow_logs_bucket_id" {
+  description = "VPC Flow logs bucket ID."
+  type        = string
+}
+
+variable "region" {
+  description = "AWS Region ID."
+  type        = map(string)
+}
+
 variable "tags" {
   description = "A map of tags to assign to the resource."
   type        = map(string)
+}
+
+variable "waf_logs_bucket_id" {
+  description = "WAFv2 logs bucket ID."
+  type        = string
 }

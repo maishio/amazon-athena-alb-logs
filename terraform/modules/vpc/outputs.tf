37a1a8de058da7d83dc89233248ce5cf550e32b1
maishio <masanori.ishio119@gmail.com>
2023-04-29 07:25:33 +0900

    refactor: add flow_logs_bucket_id output variable

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -1,14 +1,19 @@
-output "vpc_id" {
-  description = "VPC ID"
-  value       = module.vpc.vpc.id
-}
-
-output "public_subnet_ids" {
-  description = "Public subnet IDs"
-  value       = module.public_subnet.subnet.*.id
+output "flow_logs_bucket_id" {
+  description = "ID of the ALB log bucket."
+  value       = module.flow_logs.s3_bucket.id
 }
 
 output "private_subnet_ids" {
-  description = "Private subnet IDs"
+  description = "Private subnet IDs."
   value       = module.private_subnet.subnet.*.id
+}
+
+output "public_subnet_ids" {
+  description = "Public subnet IDs."
+  value       = module.public_subnet.subnet.*.id
+}
+
+output "vpc_id" {
+  description = "VPC ID."
+  value       = module.vpc.vpc.id
 }
